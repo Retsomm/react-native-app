fb77ae340936f6b53dfa6534d3cb90e600c450fd
Aria <112182ssss@example.com>
2025-03-04 17:29:29 +0800

    Add theme context and implement dark/light mode support

--- crud-app/app/index.jsx
+++ crud-app/app/index.jsx
@@ -1,12 +1,56 @@
 import { Text, View, TextInput,Pressable,StyleSheet,FlatList } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import { useState } from "react";
+import { useState, useContext,useEffect } from "react";
 import { data } from "@/data/todos";
 import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+import { Inter_500Medium, useFonts} from "@expo-google-fonts/inter";
+import Octicons from '@expo/vector-icons/Octicons'
+import { ThemeContext } from "@/context/ThemeContext";
+import Animated, {LinearTransition} from 'react-native-reanimated';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {StatusBar} from 'expo-status-bar';
+import { SafeAreaView } from "react-native-safe-area-context";
 export default function Index() {
-  const [todos, setTodos] = useState(data.sort((a,b)=>b.id - a.id));
+  const [todos, setTodos] = useState([]);
   const [text, setText] = useState("");
+  const { colorScheme, setColorScheme, theme} = useContext(ThemeContext);
+  const [loaded, error] = useFonts({
+    Inter_500Medium,
+  })
+  useEffect(()=>{
+    const fetchData = async()=>{
+      try {
+        const jsonValue = await AsyncStorage.getItem('TodoApp');
+        const storageTodos = jsonValue != null?JSON.parse
+        (jsonValue):null
+        if (storageTodos && storageTodos.length){
+          setTodos(storageTodos.sort((a,b)=>b.id - a.id));
+        } else {
+          setTodos(data.sort((a,b)=>b.id - a.id));  
+        }
+      } catch (e) {
+        console.error(e);
+      }
+    }
+    fetchData();
+  },[data])
 
+  useEffect(()=>{
+    const storeDate = async()=>{
+      try{
+        const jsonValue = JSON.stringify(todos)
+        await AsyncStorage.setItem("TodoApp",jsonValue)
+      }catch(e){
+        console.error(e);
+      }
+    }
+    storeDate();
+  },[todos])
+
+  if (!loaded && !error){
+    return null
+  }
+  const styles = createStyles(theme, colorScheme);
   const addTodo = () => {
     if (text.trim()) {
       const newId = todos.length > 0? todos[0].id +1:1;
@@ -35,83 +79,101 @@ export default function Index() {
   )
   return (
     <SafeAreaProvider style={styles.container}>
-      <View style={styles.inputContainer}>
-        <TextInput
-          style={styles.input}
-          placeholder="Add a new todo"
-          placeholderTextColor="gray"
-          value={text}
-          onChangeText={setText}
+      <SafeAreaView style={{ flex: 1 }}>
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.input}
+            placeholder="Add a new todo"
+            placeholderTextColor="gray"
+            value={text}
+            onChangeText={setText}
+          />
+          <Pressable onPress={addTodo} style={styles.addButton}>
+            <Text style={styles.addButtonText}>Add</Text>
+          </Pressable>
+          <Pressable
+            onPress={()=> setColorScheme(colorScheme === 'dark' ? 'light':'dark')}
+            style={{marginLeft:10}}>
+            {colorScheme === 'dark'
+              ? <Octicons name="moon" size={36} color={theme.text}
+              selectable={undefined} style={{width:36}} /> 
+              : <Octicons name="sun" size={36} color={theme.text}
+              selectable={undefined} style={{width:36}} />  
+            }
+          </Pressable>
+        </View>
+        <Animated.FlatList
+          data={todos}
+          renderItem={renderItem}
+          keyExtractor={todo=>todo.id}
+          contentContainerStyle={{flexGrow:1}}
+          itemLayoutAnimation={LinearTransition}
+          keyboardDismissMode="on-drag"
         />
-        <Pressable onPress={addTodo} style={styles.addButton}>
-          <Text style={styles.addButtonText}>Add</Text>
-        </Pressable>
-      </View>
-      <FlatList
-        data={todos}
-        renderItem={renderItem}
-        keyExtractor={todo=>todo.id}
-        contentContainerStyle={{flexGrow:1}}
-      />
+        <StatusBar style={colorScheme ==='dark'?'light':'dark'} />
+      </SafeAreaView>
     </SafeAreaProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "black",
-  },
-  inputContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-    marginBottom: 10,
-    padding:10,
-    width:"100%",
-    maxWidth:1024,
-    marginHorizontal:"auto",
-    pointerEvents:"auto",
-  },
-  input: {
-    flex: 1,
-    borderColor: "gray",
-    borderWidth: 1,
-    borderRadius: 5,
-    marginRight: 10,
-    padding:10,
-    fontSize:18,
-    minWidth:0,
-    color:"white",
-  },
-  addButton: {
-    backgroundColor: "white",
-    padding: 10,
-    borderRadius: 5,
-  },
-  addButtonText: {
-    color: "black",
-    fontSize: 18,
-  },
-  todoItem: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    padding: 10,
-    borderBottomColor:'gray',
-    borderBottomWidth:1,
-    gap:4,
-    width:"100%",
-    maxWidth:1024,
-    marginHorizontal:"auto",
-    pointerEvents:"auto",
-  },
-  todoText:{
-    flex:1,
-    fontSize:18,
-    color:"white",
-  },
-  completedText:{
-    textDecorationLine:"line-through",
-    color:"gray",
-  }
+function createStyles(theme, colorScheme){
+  return StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: theme.background,
+    },
+    inputContainer: {
+      flexDirection: "row",
+      alignItems: "center",
+      marginBottom: 10,
+      padding:10,
+      width:"100%",
+      maxWidth:1024,
+      marginHorizontal:"auto",
+      pointerEvents:"auto",
+    },
+    input: {
+      flex: 1,
+      borderColor: "gray",
+      borderWidth: 1,
+      borderRadius: 5,
+      marginRight: 10,
+      padding:10,
+      fontSize:18,
+      fontFamily:"Inter_500Medium",
+      minWidth:0,
+      color:theme.text,
+    },
+    addButton: {
+      backgroundColor: theme.button,
+      padding: 10,
+      borderRadius: 5,
+    },
+    addButtonText: {
+      color: colorScheme === 'dark' ? "black" : "white",
+      fontSize: 18,
+    },
+    todoItem: {
+      flexDirection: "row",
+      justifyContent: "space-between",
+      alignItems: "center",
+      padding: 10,
+      borderBottomColor:'gray',
+      borderBottomWidth:1,
+      gap:4,
+      width:"100%",
+      maxWidth:1024,
+      marginHorizontal:"auto",
+      pointerEvents:"auto",
+    },
+    todoText:{
+      flex:1,
+      fontSize:18,
+      color:theme.text,
+      fontFamily:"Inter_500Medium",
+    },
+    completedText:{
+      textDecorationLine:"line-through",
+      color:"gray",
+    }
 });
+}
